a79bffab8e119e34c8d636a77c50e1514a888193
Peter Reuterås <peter@reuteras.com>
2015-05-25 21:49:59 +0200

    Added setup config file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name='pygmaps',
+    version='0.2',
+    description='Extended version of pygmaps which adds title option.',
+    author='Peter Reuterås',
+    author_email='peter@reuteras.com',
+    url='https://github.com/reuteras/pygmaps',
+    py_modules=['pygmaps'],
+)
+

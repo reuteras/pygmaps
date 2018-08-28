b22eb5229fe8add82a039b0b1a593d66c52bf6be
Peter Reuterås <peter@reuteras.com>
2018-08-28 17:08:07 +0200

    Change email

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 setup(name='pygmaps',
     version='0.2',
     description='Extended version of pygmaps which adds title option.',
-    author='Peter Reuterås',
-    author_email='peter@reuteras.com',
+    author='PR',
+    author_email='code@reuteras.se',
     url='https://github.com/reuteras/pygmaps',
     py_modules=['pygmaps'],
 )
